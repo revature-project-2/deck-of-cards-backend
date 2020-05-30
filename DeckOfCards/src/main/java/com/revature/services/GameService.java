3d6592e2d9573db5ec4f97f1e1510cd2222b3d2d
Raul Gonzalez <rgonz166@gmail.com>
2020-05-29 21:55:23 -0700

    Fix: GameService getGamesByPlayerId to return multiple games

--- DeckOfCards/src/main/java/com/revature/services/GameService.java
+++ DeckOfCards/src/main/java/com/revature/services/GameService.java
@@ -7,8 +7,8 @@ import com.revature.beans.Game;
 public interface GameService {
 	public Integer addGame(Game g);
 	public Set<Game> getAllGames();
+	public Set<Game> getGamesByPlayerId(Integer id);
 	public Game getGameById(Integer id);
-	public Game getGameByPlayerId(Integer id);
 	public void updateGame(Game g);
 	public void deleteGame(Game g);
 }
