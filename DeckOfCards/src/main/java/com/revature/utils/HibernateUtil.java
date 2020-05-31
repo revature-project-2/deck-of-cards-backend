674cc5419afd8e4bade81b8fdc387a3cad0ecd27
Raul Gonzalez <rgonz166@gmail.com>
2020-05-30 20:23:57 -0700

    Changed: HibernateUtil

--- DeckOfCards/src/main/java/com/revature/utils/HibernateUtil.java
+++ DeckOfCards/src/main/java/com/revature/utils/HibernateUtil.java
@@ -7,33 +7,35 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
 
 public class HibernateUtil {
-private static SessionFactory sessionFactory;
+	private static HibernateUtil hu;
+	private static SessionFactory sessionFactory;
 	
-	private static SessionFactory buildSessionFactory() {
-        try {
-        	Configuration configuration = new Configuration();
-        	configuration.configure("hibernate.cfg.xml");
-        	System.out.println("Hibernate Annotation Configuration loaded");
-        	
-        	ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
-        	System.out.println("Hibernate Annotation serviceRegistry created");
-        	
-        	SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-        return sessionFactory;
-
-        }
-        catch (Exception ex) {
-           ex.printStackTrace();
-        }
-        return null;
-    }
+	private HibernateUtil() {
+		super();
+	}
+	public synchronized static HibernateUtil getHibernateUtil() {
+		if (hu == null) {
+			hu = new HibernateUtil();
+		}
+		return hu;
+	}
 	
-	public static SessionFactory getSessionFactory() {
-		if(sessionFactory == null) sessionFactory = buildSessionFactory();
-        return sessionFactory;
-    }
+	public synchronized static SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
+			StandardServiceRegistry standardRegistry =
+					new StandardServiceRegistryBuilder().configure().build();
+			Metadata meta = new MetadataSources(standardRegistry)
+					.getMetadataBuilder()
+					.applyImplicitNamingStrategy(ImplicitNamingStrategyJpaCompliantImpl.INSTANCE)
+					.build();
+			sessionFactory = meta.getSessionFactoryBuilder().build();
+		}
+		return sessionFactory;
+	}
+	
+	public Session getSession() {
+		return this.getSessionFactory().openSession();
+	}
 }
