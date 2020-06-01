2bfe21eeb80842ee46175321c4c0b8bbde3176f7
Raul Gonzalez <rgonz166@gmail.com>
2020-06-01 14:09:41 -0700

    Changing stuff for player but not needed

--- DeckOfCards/src/main/java/com/revature/services/PlayerServiceImpl.java
+++ DeckOfCards/src/main/java/com/revature/services/PlayerServiceImpl.java
@@ -12,7 +12,7 @@ import com.revature.data.GameDAO;
 import com.revature.data.GameHibernate;
 import com.revature.data.PlayerDAO;
 
-//import jdk.internal.net.http.common.Log;
+import jdk.internal.net.http.common.Log;
 
 @Service
 public class PlayerServiceImpl implements PlayerService {
@@ -35,18 +35,18 @@ public class PlayerServiceImpl implements PlayerService {
 	@Override
 	public Set<Player> getAllPlayers() {
 		Set<Player> people = pDao.getAll();
-//		for (Player p : people) {
-//			if (p.getGames() == null)
-//				p.setGames(gDao.getGamesByPlayer(p.getId()));
-//		}
+		for (Player p : people) {
+			if (p.getGames() == null)
+				p.setGames(gDao.getGamesByPlayer(p.getId()));
+		}
 		return people;
 	}
 
 	@Override
 	public Player getPlayerById(Integer id) {
 		Player p = pDao.getById(id);
-//		if (p.getGames() == null)
-//			p.setGames(gDao.getGamesByPlayer(p.getId()));
+		if (p.getGames() == null)
+			p.setGames(gDao.getGamesByPlayer(p.getId()));
 		return p;
 	}
 
@@ -55,9 +55,9 @@ public class PlayerServiceImpl implements PlayerService {
 		log.trace("Get player by uname: "+username);
 		Player p = pDao.getPlayerByUsernameAndPassword(username, password); // getting to this stage
 		log.trace("Player: " + p);
-//		if (p != null && p.getGames() == null) {
-//			p.setGames(gDao.getGamesByPlayer(p.getId()));
-//		}
+		if (p != null && p.getGames() == null) {
+			p.setGames(gDao.getGamesByPlayer(p.getId()));
+		}
 		return p;
 	}
 
@@ -68,9 +68,9 @@ public class PlayerServiceImpl implements PlayerService {
 
 	@Override
 	public void deletePlayer(Player p) {
-//		for (Game g : p.getGames()) {
-//			gDao.delete(g);
-//		}
+		for (Game g : p.getGames()) {
+			gDao.delete(g);
+		}
 		pDao.delete(p);
 	}
 
