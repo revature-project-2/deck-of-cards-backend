dd8e662945537221f76016956bcff25a282545a0
Multezem Kedir <multekedir@users.noreply.github.com>
2020-06-05 07:39:35 -0700

    changed CrossOrigin for s3

--- DeckOfCards/src/main/java/com/revature/controller/LoginController.java
+++ DeckOfCards/src/main/java/com/revature/controller/LoginController.java
@@ -11,7 +11,7 @@ import com.revature.services.PlayerService;
 
 @RestController
 @RequestMapping(path="/login")
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins="http://cardgamesprod.s3-website-us-west-2.amazonaws.com")
 public class LoginController {
 	private PlayerService pServ;
 	
